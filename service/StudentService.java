98644f37f254e23d51c28d717363a333b4542003
Bhushan Patil <106161731+Bhushanrpatil02@users.noreply.github.com>
2022-09-27 11:53:00 +0530

    Add files via upload

---
+++ service/StudentService.java
@@ -0,0 +1,21 @@
+package com.demo.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.jaxb.SpringDataJaxb.SortDto;
+
+import com.demo.model.Student;
+
+public interface StudentService {
+
+	public String saveEmp(Student s);
+
+	public Student getStudent(int rollno);
+
+	public List<Student> getAllStudent();
+
+	public String deletStudent(int rollno);
+
+	public String updateStudent(Student s);
+
+}
